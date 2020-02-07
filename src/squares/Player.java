ec660bad2a612d29493c9c199a8d7621ccea7ff3
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2020-02-07 16:58:29 -0600

    Initialized some fields

--- src/squares/Player.java
+++ src/squares/Player.java
@@ -6,7 +6,7 @@ public class Player {
     public static final int itime = 10;
     public static final int PRACTICE_MODE_LIVES = 100;
 
-    public CharacterState charState;
+    public CharacterState charState = CharacterState.NORMAL;
     public int xPosition; //Target position of the character in grid coordinates
     public int yPosition;
     public int xTarg; // Target position of the character in panel coordinates
@@ -14,9 +14,9 @@ public class Player {
     public int xCoordinates; //Position of the upper left hand corner of the character pic in panel coordinates
     public int yCoordinates;
 
-    public int iftime;
-    public int hp;
-    public boolean isPracticeMode;
+    public int iftime = 0;
+    public int hp = 1;
+    public boolean isPracticeMode = false;
 
     public boolean isInvincible(int cframe) {
         return iftime + itime >= cframe;
